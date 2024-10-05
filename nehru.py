1e7a0bc8759b7c270cd8ca4e9d33c370f40fadae
Yaiphaba-Ng <yaiphabalamzing@gmail.com>
2024-10-05 18:07:39 +0530

    nehru.py is remote server backend from Da Nehru

---
+++ nehru.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
+import shutil
+import os
+
+
+import json
+from pathlib import Path
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+origins = [ "http://localhost", "http://localhost:3000","http://localhost:5173", "http://ocr_api.lamzingtech.com"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Allows all hosts
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+# Mount the static files directory
+app.mount("/uploads", StaticFiles(directory=UPLOAD_DIR), name="uploads")
+
+@app.get("/load-json")
+async def load_json():
+    # Path to your JSON file
+    json_file_path = Path("data.json")
+
+    # Load the JSON file
+    try:
+        with json_file_path.open() as f:
+            data = json.load(f)
+        return JSONResponse(content=data)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+    
+
+@app.post("/upload-image/")
+async def upload_image(file: UploadFile = File(...)):
+    try:
+        file_location = os.path.join(UPLOAD_DIR, file.filename)
+        with open(file_location, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        # Return the URL to access the uploaded image
+        image_url = f"https://ocr_api.lamzingtech.com/uploads/{file.filename}"
+
+        json_file_path = Path("data.json")
+
+    # Load the JSON file
+      
+        with json_file_path.open() as f:
+            data = json.load(f)
+        return JSONResponse(content={"image": image_url, "data":data})
+    
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
